88dcd07265a8fc69a9ee5f1e6542e978b7c93bb2
mailinh0806 <97440936+mailinh0806@users.noreply.github.com>
2022-02-21 18:59:02 +0700

    Add files via upload

---
+++ bài thực hành 21012022/baithuchanh21-02/src/App35.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class App35 {
+    
+        public static void main(String[] args) throws Exception{
+           
+        Scanner sc=new Scanner(System.in);
+        System.out.println("num 1: ");
+        int a=sc.nextInt();
+        System.out.println("num  2: ");
+        int b=sc.nextInt();
+        if(a>b)
+        {
+            System.out.println("max is: "+a);
+        }
+        else
+        {
+            System.out.println("max is: "+b);
+        }
+
+
+    
+    
+        }
+    }
+    
+
